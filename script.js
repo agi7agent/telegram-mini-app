b109839c10de6c166269888c1a49ec1ce2016974
agi7agent <agi7agent@gmail.com>
2025-03-17 20:49:19 +0300

    Update script.js

--- script.js
+++ script.js
@@ -45,17 +45,3 @@ document.getElementById('aboutSection').style.display = 'none';
 }
 
 
-
-
-function doPost(e) {
-    const data = JSON.parse(e.postData.contents);
-    const name = data.name;
-    const phone = data.phone;
-
-    // Записываем данные в таблицу
-    const sheet = SpreadsheetApp.getActive().getSheetByName("Бронирования");
-    sheet.appendRow([name, phone, new Date()]);
-
-    return ContentService.createTextOutput("OK")
-        .setMimeType(ContentService.MimeType.TEXT);
-}
